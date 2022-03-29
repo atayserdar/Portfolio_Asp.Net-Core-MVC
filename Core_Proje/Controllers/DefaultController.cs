c884d18eef644bf41a210a03d69385bfe21ffa9a
Serdar Atay <atayserdar@gmail.com>
2022-03-29 11:24:52 +0300

    Update SendMessage and Index.cshtml
    Başlangıçta ViewComponent olarak kullandığımız SendMessage yerine PartialView yöntemi kullandım.

--- Core_Proje/Controllers/DefaultController.cs
+++ Core_Proje/Controllers/DefaultController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace Core_Proje.Controllers
 {
@@ -16,6 +20,24 @@ namespace Core_Proje.Controllers
 
         public PartialViewResult NavbarPartial()
         {
+            return PartialView();
+        }
+
+        [HttpGet]
+        public PartialViewResult SendMessage()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult SendMessage(Message message)
+        {
+            MessageManager messageManager = new MessageManager(new EfMessageDal());
+
+            //Mesajın kaydedildiği andaki tarihi DB ye yansıtılsın.
+            message.Date = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            message.Status = true; // Başlangıçta aktif olsun. Okuduktan sonra false a çekeceğiz.
+            messageManager.TAdd(message);
             return PartialView();
         }
     }
